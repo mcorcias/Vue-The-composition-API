53e153f18bd78b35ad86b3bd8df744fc4a612652
mcorcias <mosescorcias@gmail.com>
2021-01-30 18:17:37 +0200

    This project is about composition API - using in setup function, useRoute, composition functions, async await, watcEffect

---
+++ blog-posts/src/views/Tag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tag">
+      <div v-if="error">{{error}}</div>
+      <div v-if="posts.length" class="layout">
+          <PostList :posts="postWithTag"/>
+          <TagCloud :posts="posts" />
+      </div>
+      <div v-else>
+          <Spinner />
+      </div>
+  </div>
+</template>
+
+<script>
+import Spinner from '../components/Spinner.vue'
+import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
+import getPosts from '../composables/getPosts'
+import {useRoute} from 'vue-router'
+import { computed } from 'vue'
+
+export default {
+    components:{PostList,Spinner,TagCloud},
+    setup(){
+        const route = useRoute()
+        const {posts,error,load}=getPosts()
+        load()
+
+        const postWithTag = computed(()=>{
+            return posts.value.filter(p=>p.tags.includes(route.params.tag))
+        })
+
+        return {error,posts,postWithTag}
+    }
+}
+</script>
+
+<style>
+    .tag{
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 10px;
+    }
+</style>
